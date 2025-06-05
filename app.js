a5a4c05fbeaa6a81e5e5bf411c3bd8aceeba87ef
slavon213 <slavon213@gmail.com>
2025-06-05 12:50:04 +0300

    commit to git

---
+++ app.js
@@ -0,0 +1,59 @@
+let draggedItem = null;
+
+const liItems = document.querySelectorAll("li");
+liItems.forEach((item) => {
+    item.setAttribute("draggable", "true");
+    item.addEventListener("dragstart", () => {
+        draggedItem = item;
+        item.style.opacity = ".5";
+        item.style.cursor = "grabbing";
+    });
+    item.addEventListener("dragend", () => {
+        item.style.opacity = "1";
+        item.style.cursor = "grab";
+    });
+});
+
+
+const containers = document.querySelectorAll(".container");
+
+containers.forEach((container) => {
+    let innerUl = container.querySelector("ul");
+    const animatedWords = document.querySelector("h1 span");
+    
+
+    container.addEventListener("dragover", (e) => {
+        container.classList.add("container-drag-over");
+        animatedWords.classList.add("animated");
+        e.preventDefault();
+    });
+    container.addEventListener("dragleave", (e) => {
+        container.classList.remove("container-drag-over");
+    });
+
+    container.addEventListener("drop", (e) => {
+        if (draggedItem && innerUl) {
+            // innerUl.appendChild(draggedItem);
+            innerUl.insertBefore(draggedItem, innerUl.firstChild);
+        }
+        animatedWords.classList.remove("animated");
+        container.classList.remove("container-drag-over");
+    });
+});
+
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const parentEl = button.closest(".container");
+        const ulElement = parentEl.querySelector("ul");
+        const liElems = [...ulElement.children];
+        console.log("\nTotal elements: " + liElems.length);
+
+        for (const element of liElems) {
+            console.log(element.textContent);
+        }
+    });
+});
